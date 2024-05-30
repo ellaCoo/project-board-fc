d989fb7f94242581ed52f675daf6b90f5eb502ff
jglorys <jglorys@naver.com>
2024-05-31 00:39:51 +0900

    #5 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/fc/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fc/projectboard/repository/ArticleCommentRepository.java
@@ -10,12 +10,15 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface ArticleCommentRepository extends
         JpaRepository<ArticleComment, Long>,
         QuerydslPredicateExecutor<ArticleComment>, // entity 안에 있는 모든 필드에 대한 기본 검색 기능 추가
         QuerydslBinderCustomizer<QArticleComment>  // 부분 검색
 {
+    List<ArticleComment> findByArticle_Id(Long articleId);
     @Override
     default void customize(QuerydslBindings bindings, QArticleComment root) {
         bindings.excludeUnlistedProperties(true);
--- src/main/java/com/fc/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fc/projectboard/repository/ArticleRepository.java
@@ -2,10 +2,10 @@ package com.fc.projectboard.repository;
 
 import com.fc.projectboard.domain.Article;
 import com.fc.projectboard.domain.QArticle;
-import com.fc.projectboard.domain.QArticleComment;
 import com.querydsl.core.types.dsl.DateTimeExpression;
-import com.querydsl.core.types.dsl.SimpleExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -18,6 +18,7 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>, // entity 안에 있는 모든 필드에 대한 기본 검색 기능 추가
         QuerydslBinderCustomizer<QArticle>  // 부분 검색
 {
+    Page<Article> findByTitle(String title, Pageable pageable);
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
         bindings.excludeUnlistedProperties(true);
