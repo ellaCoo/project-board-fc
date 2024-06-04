0ce599b3b330506d846c435d83018bfaf3e2ea08
jglorys <jglorys@naver.com>
2024-06-04 17:04:19 +0900

    #5 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현하고
    데이터 전달 완료
    게시판 페이지 기본 정렬 속성 부여: 10개 1페이지, 시간 내림차순

--- src/main/java/com/fc/projectboard/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/fc/projectboard/dto/response/ArticleWithCommentsResponse.java
@@ -8,7 +8,7 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public record ArticleWithCommentResponse(
+public record ArticleWithCommentsResponse(
         Long id, // 게시글의 고유 번호(ID)
         String title, // 게시글 제목
         String content, // 게시글 내용
@@ -16,23 +16,23 @@ public record ArticleWithCommentResponse(
         LocalDateTime createdAt, // 게시글이 생성된 시간
         String email, // 게시글 작성자의 이메일 주소
         String nickname, // 게시글 작성자의 닉네임
-        Set<ArticleCommentResponse> articleCommentResponses // 게시글에 달린 댓글 목록, ArticleCommentResponse 객체의 집합
+        Set<ArticleCommentResponse> articleCommentsResponses // 게시글에 달린 댓글 목록, ArticleCommentResponse 객체의 집합
 ) implements Serializable { // Serializable 인터페이스를 구현하여 객체의 직렬화가 가능하도록 함
 
     // 정적 팩토리 메서드, 필요한 모든 정보를 받아 ArticleWithCommentResponse 객체를 생성하고 반환
-    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
-        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
+    public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
     }
 
     // DTO를 사용해 ArticleWithCommentResponse 객체를 생성하는 정적 팩토리 메서드
-    public static ArticleWithCommentResponse from(ArticleWithCommentsDto dto) {
+    public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto) {
         String nickname = dto.userAccountDto().nickname(); // DTO에서 사용자 닉네임 가져오기
         if (nickname == null || nickname.isBlank()) { // 닉네임이 null이거나 비어있으면
             nickname = dto.userAccountDto().userId(); // 대체 정보로 사용자 ID 사용
         }
 
         // 생성자를 통해 새로운 ArticleWithCommentResponse 객체를 생성하고 초기화하여 반환
-        return new ArticleWithCommentResponse(
+        return new ArticleWithCommentsResponse(
                 dto.id(), // DTO에서 게시글 ID 가져오기
                 dto.title(), // DTO에서 게시글 제목 가져오기
                 dto.content(), // DTO에서 게시글 내용 가져오기
