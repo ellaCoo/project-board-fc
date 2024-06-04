529ef6fe03cc1594d8e1959d161f38ef19242707
jglorys <jglorys@naver.com>
2024-06-04 17:03:27 +0900

    #5 - 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- src/main/java/com/fc/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fc/projectboard/repository/ArticleRepository.java
@@ -18,7 +18,11 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>, // entity 안에 있는 모든 필드에 대한 기본 검색 기능 추가
         QuerydslBinderCustomizer<QArticle>  // 부분 검색
 {
-    Page<Article> findByTitle(String title, Pageable pageable);
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining (String content, Pageable pageable);
+    Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+    Page<Article> findByHashtag(String hashtag, Pageable pageable);
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
         bindings.excludeUnlistedProperties(true);
