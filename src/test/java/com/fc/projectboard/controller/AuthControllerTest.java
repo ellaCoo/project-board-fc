599a7351ec5e0b6d55f2591ed15c7803b265cdb5
jglorys <jglorys@naver.com>
2024-06-05 11:15:06 +0900

    #6 - 예제 문자 삭제, 로그인 테스트 문제 해결
    부트스트랩 예제 코드를 사용하면서 복사된 예제 문자열 삭제
    `/login` 페이지는 직접 만든 컨트롤러가 아니고
    스프링 시큐리티가 해주는 작업이므로
    컨트롤러 테스트에서 읽어야 할 컨트롤러 빈이 없음
    이를 `Void.class`로 표현

--- src/test/java/com/fc/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/fc/projectboard/controller/AuthControllerTest.java
@@ -8,13 +8,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("View 컨트롤러 - 인증")
 @Import(SecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(Void.class) // Void.class : 컨트롤러 테스트에서 읽어야 할 컨트롤러 빈이 없음
 public class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -31,6 +33,7 @@ public class AuthControllerTest {
         // When & Then
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andDo(MockMvcResultHandlers.print());
     }
 }
