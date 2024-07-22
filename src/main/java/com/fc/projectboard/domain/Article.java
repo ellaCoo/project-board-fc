d423686b2889b3882215097529eaff621ab499a3
jglorys <jglorys@naver.com>
2024-07-22 16:37:13 +0900

    #67 - data.sql 파일 수정 및 QueryDSL 수정

--- src/main/java/com/fc/projectboard/domain/Article.java
+++ src/main/java/com/fc/projectboard/domain/Article.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
