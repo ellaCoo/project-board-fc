5d03a45ec7af37f8a6e700480b78ef0858d043f5
jglorys <jglorys@naver.com>
2024-07-22 16:43:33 +0900

    #67 - 리팩토링:소스코드 정리
    * 애노테이션 줄 위치 정리
    * 부연 설명이 필요하다 여겨지는 부분에 주석 추가
    * 컨트롤러 테스트 코드 일부에 들여쓰기가 잘못되어 수정

--- src/main/java/com/fc/projectboard/dto/response/ArticleResponse.java
+++ src/main/java/com/fc/projectboard/dto/response/ArticleResponse.java
@@ -7,41 +7,37 @@ import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-// ArticleResponse는 레코드 클래스로, 응답 데이터 구조를 간단하게 정의
 public record ArticleResponse(
-        Long id, // 게시글의 ID
-        String title, // 게시글의 제목
-        String content, // 게시글의 내용
+        Long id,
+        String title,
+        String content,
         Set<String> hashtags,
-        LocalDateTime createdAt, // 게시글 생성 시간
-        String email, // 게시글 작성자의 이메일
-        String nickname // 게시글 작성자의 닉네임
+        LocalDateTime createdAt,
+        String email,
+        String nickname
 ) {
 
-    // 정적 팩토리 메서드, 모든 필드 값을 입력받아 새로운 ArticleResponse 객체를 생성하여 반환
     public static ArticleResponse of(Long id, String title, String content, Set<String> hashtags, LocalDateTime createdAt, String email, String nickname) {
         return new ArticleResponse(id, title, content, hashtags, createdAt, email, nickname);
     }
 
-    // DTO 객체로부터 ArticleResponse 객체를 생성하는 정적 팩토리 메서드
     public static ArticleResponse from(ArticleDto dto) {
-        String nickname = dto.userAccountDto().nickname(); // DTO에서 닉네임을 추출
-        if (nickname == null || nickname.isBlank()) { // 닉네임이 비어있거나 null일 경우
-            nickname = dto.userAccountDto().userId(); // 대체 정보로 유저 ID 사용
+        String nickname = dto.userAccountDto().nickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.userAccountDto().userId();
         }
 
-        // 생성자를 통해 ArticleResponse 객체 생성 및 필드 값 설정, 반환
         return new ArticleResponse(
-                dto.id(), // 게시글 ID
-                dto.title(), // 게시글 제목
-                dto.content(), // 게시글 내용
+                dto.id(),
+                dto.title(),
+                dto.content(),
                 dto.hashtagDtos().stream()
                         .map(HashtagDto::hashtagName)
                         .collect(Collectors.toUnmodifiableSet())
                 ,
-                dto.createdAt(), // 게시글 생성 시간
-                dto.userAccountDto().email(), // 게시글 작성자 이메일
-                nickname // 계산된 또는 대체된 닉네임
+                dto.createdAt(),
+                dto.userAccountDto().email(),
+                nickname
         );
     }
 
