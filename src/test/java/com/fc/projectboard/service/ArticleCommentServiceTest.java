85b385601e72475987a98d8e32089c58ab6f82dd
jglorys <jglorys@naver.com>
2024-08-02 03:06:08 +0900

    #104 - Thymeleaf 문법에서 틀린 부분 수정
    기존 코드가 잘 작동하고 있어서 발견하지 못했으나,
    문법적 오류가 있었고,
    이번 버전 업 후 테스트 도중 발견됨.
    이를 수정함.

--- src/test/java/com/fc/projectboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/fc/projectboard/service/ArticleCommentServiceTest.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
 
