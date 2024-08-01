6120191f85ac0433e01384b7386100dc75c92d45
jglorys <jglorys@naver.com>
2024-08-02 01:38:43 +0900

    #90 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/test/java/com/fc/projectboard/config/TestSecurityConfig.java
+++ src/test/java/com/fc/projectboard/config/TestSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.fc.projectboard.config;
 
-import com.fc.projectboard.domain.UserAccount;
-import com.fc.projectboard.repository.UserAccountRepository;
+import com.fc.projectboard.dto.UserAccountDto;
+import com.fc.projectboard.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -14,18 +14,26 @@ import static org.mockito.BDDMockito.given;
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
 
-    @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetUp() {
         // 각 테스트 실행 직전에 인증정보 넣어주라고 작성함
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
             "ellaTest",
             "pw",
             "ella-test@mail.com",
             "ella-test",
             "test memmo"
-        )));
+        );
 
     }
 }
